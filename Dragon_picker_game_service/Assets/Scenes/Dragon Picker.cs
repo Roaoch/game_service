a8c051ec31b7864da2d770f5ddffd1747a54ed0f
Azicel <Elenapor12@gmail.com>
2023-12-19 21:42:55 +0500

    Спавн элементов

--- Dragon_picker_game_service/Assets/Scenes/Dragon Picker.cs
+++ Dragon_picker_game_service/Assets/Scenes/Dragon Picker.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,6 +12,11 @@ public class DragonPicker : MonoBehaviour
     [SerializeField] private float energyShieldBottomY = -6f;
     [SerializeField] private float energyShieldRadius = 1.5f;
     [SerializeField] private List<GameObject> shieldList;
+    [SerializeField] private List<int> elementsInInventoryCount;
+    [SerializeField] private List<GameObject> elementsList;
+    [SerializeField] private int elementsCount;
+    private int timeBetweenSpawnElements = 5;
+    private float time = 0;
 
     void Start()
     {
@@ -27,7 +34,13 @@ public class DragonPicker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        time = time + Time.deltaTime;
+        if (time >= timeBetweenSpawnElements)
+        {
+            Debug.Log("Time passed");
+            SpawnElement();
+            time = 0;
+        }
     }
 
     public void DragonEggDestroy()
@@ -40,5 +53,15 @@ public class DragonPicker : MonoBehaviour
         {
             SceneManager.LoadScene("_0Scene");
         }
+    }
+
+    public void SpawnElement()
+    {
+        Debug.Log("element must be spawned");
+        var r = new System.Random();
+        var element = r.Next(0,4);
+        var coordinatesWidth = r.Next(-19, 19);
+        var coordinatesHeight = r.Next(-9, 9);
+        Instantiate(elementsList[element], new Vector3(coordinatesWidth, coordinatesHeight, 0), new Quaternion(0,0,0,0));
     }
 }
